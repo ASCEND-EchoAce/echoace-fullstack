cdf3af15491c050dc6fdeef2b16e891513f569c9
Chris Chen <cchen6479@gmail.com>
2024-12-31 17:03:10 -0500

    delete tutorial files

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
-import Hero from '@/components/hero';
-import ConnectSupabaseSteps from '@/components/tutorial/connect-supabase-steps';
-import SignUpUserSteps from '@/components/tutorial/sign-up-user-steps';
-import { hasEnvVars } from '@/utils/supabase/check-env-vars';
-
 export default async function Index() {
   return (
     <>
-      <Hero />
+      <section id="landing hero" className='flex flex-col justify-center items-center h-96 gap-4'>
+        <h1 className="text-8xl font-semibold">
+          EchoAce
+        </h1>
+        <h2>Subtitle</h2>
+      </section>
       <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+        <section id="quotes">
+
+        </section>
       </main>
     </>
   );
