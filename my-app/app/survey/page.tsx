a45fc75300789a4fe5d97b402f0797ad8a5c0b4b
Shiloh <82426376+SnowiiHDR@users.noreply.github.com>
2025-01-12 20:06:42 -0500

    Cleanup
    Modified favicon to current EchoAce placeholder.
    Changed link from /notes to /survey
    Changed Title to EchoAce

--- my-app/app/survey/page.tsx
+++ my-app/app/survey/page.tsx
@@ -8,13 +8,14 @@ export default function Page() {
   const [notes, setNotes] = useState<any[] | null>(null);
   const supabase = createClient();
 
-  useEffect(() => {
-    const getData = async () => {
-      const { data } = await supabase.from('notes').select();
-      setNotes(data);
-    };
-    getData();
-  }, []);
+  //   useEffect(() => {
+  //     const getData = async () => {
+  //       const { data } = await supabase.from('notes').select();
+  //       setNotes(data);
+  //     };
+  //     getData();
+  //   }, []);
+  // If we want to actually use sql and tables (backend?)
 
   return (
     <div>
