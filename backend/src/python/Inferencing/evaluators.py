f41f75b3448f6e088d2cbf5cc74db61c31066b9b
Skai Nzeuton <san82@cornell.edu>
2025-01-17 10:11:32 +0700

    Implement get_feedback method to extract the generated feedback from the rest of the model outputs
    - evaluate method now only returns the generated feedback instead of the entire chat history
    - removed unnecessary instance variables from BasicEvaluator class

--- backend/src/python/Inferencing/evaluators.py
+++ backend/src/python/Inferencing/evaluators.py
@@ -4,15 +4,21 @@ from transformers import pipeline
 class BasicEvaluator:
     def __init__(self):
         self.pipe = pipeline("text-generation", model="meta-llama/Llama-3.2-1B-Instruct", torch_dtype=torch.bfloat16, device_map="auto")
+
+    def get_feedback(self, generated_text):
+        header = "<|start_header_id|>assistant<|end_header_id|>"
+        header_index = generated_text.index(header)
+        feedback_index = header_index + len(header) + 2
+        return generated_text[feedback_index:]
         
     def evaluate(self, question, response):
-        self.sys_content = f"""
+        sys_content = f"""
             You are conducting a behavioral interview for the 'Software Engineering Intern' position at LinkedIn.
             
             To the candidate you are interviewing, you asked the question '{question}'.
             """
         
-        self.user_content = f"""                    
+        user_content = f"""                    
                     Evaluate the candidate's reponse on a scale of 1 to 5, where 5 is the highest rating, and briefly explain why you gave the candidate that score.
                     
                     Here is the candidate's response:
@@ -23,15 +29,15 @@ class BasicEvaluator:
         messages = [
             {
                 "role": "system",
-                "content": self.sys_content,
+                "content": sys_content,
             },
             {
                 "role": "user",
-                "content": self.user_content
+                "content": user_content
             }
         ]
 
-        self.prompt = self.pipe.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+        prompt = self.pipe.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
 
-        outputs = self.pipe(self.prompt, max_new_tokens=256)
-        return outputs[0]["generated_text"]
+        outputs = self.pipe(prompt, max_new_tokens=256)
+        return self.get_feedback(outputs[0]["generated_text"])
