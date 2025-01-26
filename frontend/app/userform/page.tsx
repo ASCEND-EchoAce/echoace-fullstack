2983aa96848d0f00c2e4d212cafbed72cb3956d4
Jeremy Cortez <realcortez15@gmail.com>
2025-01-26 12:17:25 -0500

    implemented dashbaord, navbar

---
+++ frontend/app/userform/page.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { createClient } from '@/utils/supabase/client';
+import { useEffect, useState } from 'react';
+import SurveyForm from '@/components/SurveyForm';
+
+export default function Page() {
+  // const [notes, setNotes] = useState<any[] | null>(null);
+  // const supabase = createClient();
+
+  //   useEffect(() => {
+  //     const getData = async () => {
+  //       const { data } = await supabase.from('notes').select();
+  //       setNotes(data);
+  //     };
+  //     getData();
+  //   }, []);
+  // If we want to actually use sql and tables (backend?)
+
+  return (
+    <div>
+      <h1 style={{ fontSize: '3rem', textAlign: 'center', color: '#fff', marginBottom: '20px' }}>
+        Opening Survey
+      </h1>
+      <p></p>
+
+      {/* Displaying the notes from the Supabase database 
+        <pre>{JSON.stringify(notes, null, 2)}</pre>
+        */}
+      {/* Render Survey Component */}
+      <SurveyForm />
+
+      {/* Add any other content here */}
+      <p></p>
+      <footer>
+        <p></p>
+      </footer>
+    </div>
+  );
+}
