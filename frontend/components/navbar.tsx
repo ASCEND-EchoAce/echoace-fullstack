c5276a6cd9718b1dd7a167dabd9d47e2915bc745
jer-cortez <realcortez15@gmail.com>
2025-02-08 17:41:05 -0500

    Navbar blue (#10)
    * camara Integration
    
    * updated navbar
    
    * made corrections

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -2,39 +2,41 @@
 import Link from 'next/link'; // For routing between pages
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'; // If you want a Radix-based dropdown
 import { useRouter } from 'next/navigation'; // For programmatic navigation (optional)
+import { useState, useEffect } from "react";
 
 const Navbar = () => {
   const router = useRouter();
 
   return (
-    <nav className="text-white p-4 shadow-md">
+    <nav className="text-white p-4">
       <div className="max-w-screen-xl mx-auto flex justify-between items-center">
         {/* Middle: Navigation Links */}
         <div className="hidden sm:flex space-x-6">
-          <Link href="/landing" className="hover:text-gray-300">
+          <Link href="/landing" className="text-black dark:text-white hover:text-blue-500 dark:hover:text-blue-500">
             Home
           </Link>
-          <Link href="/about" className="hover:text-gray-300">
+          <Link href="/about" className="text-black dark:text-white hover:text-blue-500 dark:hover:text-blue-500">
             About
           </Link>
-          <Link href="/survey" className="hover:text-gray-300">
+          <Link href="/userform" className="text-black dark:text-white hover:text-blue-500 dark:hover:text-blue-500">
             Survey
           </Link>
-          <Link href="/interview" className="hover:text-gray-300">
+          <Link href="/interview" className="text-black dark:text-white hover:text-blue-500 dark:hover:text-blue-500">
             Interview
           </Link>
         </div>
 
         {/* Right side: Dropdown or Profile (optional) */}
-        <div className="sm:flex hidden items-center space-x-4">
+        <div className="text-black dark:text-white sm:flex hidden items-center space-x-4">
           <DropdownMenu.Root>
             <DropdownMenu.Trigger asChild>
-              <button className="px-4 py-2 text-sm font-medium rounded-md">Options</button>
+              <button className="text-black dark:text-white hover:text-blue-500 dark:hover:text-blue-500 px-5 py-2 text-sm">Options</button>
             </DropdownMenu.Trigger>
-            <DropdownMenu.Content className="rounded-md shadow-lg p-2 mt-2">
-              <DropdownMenu.Item className="p-2 text-white">My Profile</DropdownMenu.Item>
-              <DropdownMenu.Item className="p-2 text-white">Settings</DropdownMenu.Item>
-              <DropdownMenu.Item className="p-2 text-white">Log out</DropdownMenu.Item>
+            <DropdownMenu.Content className="text-black dark:text-white rounded-md shadow-lg p-2 mt-2">
+              <DropdownMenu.Item className="p-2 hover:bg-gray-200 dark:hover:bg-gray-700 cursor-pointer">My Profile</DropdownMenu.Item>
+              <DropdownMenu.Item className="p-2 hover:bg-gray-200 dark:hover:bg-gray-700 cursor-pointer">Settings</DropdownMenu.Item>
+              <DropdownMenu.Item className="p-2 hover:bg-gray-200 dark:hover:bg-gray-700 cursor-pointer">
+                <Link href="../sign-in">Log out</Link></DropdownMenu.Item>
             </DropdownMenu.Content>
           </DropdownMenu.Root>
         </div>
