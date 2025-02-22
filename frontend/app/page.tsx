b45818b5ebd71a916960c553dea1a36fd59f4428
Zion S <szion817@gmail.com>
2025-02-22 14:36:46 -0500

    Fixed Links and Theming
    -Changed some header links to just go back to landing page '/'
    -added "Your solution to skill building." on landing page.
    -Fixed theming for survey. Light, dark, and system work.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,7 +3,7 @@ export default async function Index() {
     <>
       <section id="landing hero" className="flex flex-col justify-center items-center h-96 gap-4">
         <h1 className="text-8xl font-semibold">EchoAce</h1>
-        <h2>Subtitle</h2>
+        <h2>Your solution to skill building.</h2>
       </section>
       <main className="flex-1 flex flex-col gap-6 px-4">
         <section id="quotes"></section>
--- frontend/app/userform/page.tsx
+++ frontend/app/userform/page.tsx
@@ -1,9 +1,7 @@
 'use client';
 
-import { createClient } from '@/utils/supabase/client';
-import { useEffect, useState } from 'react';
 import SurveyForm from '@/components/SurveyForm';
-
+import { useTheme } from 'next-themes';
 export default function Page() {
   // const [notes, setNotes] = useState<any[] | null>(null);
   // const supabase = createClient();
@@ -17,6 +15,13 @@ export default function Page() {
   //   }, []);
   // If we want to actually use sql and tables (backend?)
 
+  const{theme} = useTheme();
+
+  let titleStyle;
+  if (theme === 'dark') titleStyle = { fontSize: '3rem', textAlign: 'center', color: '#fff', marginBottom: '20px' };
+   else {
+    titleStyle = { fontSize: '3rem',textAlign: 'center', color: '#000', marginBottom: '20px' }
+   }
   return (
     <div>
       <h1 style={{ fontSize: '3rem', textAlign: 'center', color: '#fff', marginBottom: '20px' }}>
