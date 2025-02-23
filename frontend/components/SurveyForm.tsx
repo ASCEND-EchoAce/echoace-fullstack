e2f632b566e912e814160dc5ddb158dd64ccb7c6
Jeremy Cortez <realcortez15@gmail.com>
2025-02-22 21:22:50 -0500

    more changes for conflict

--- frontend/components/SurveyForm.tsx
+++ frontend/components/SurveyForm.tsx
@@ -2,10 +2,25 @@ import 'survey-core/defaultV2.min.css';
 import { Model } from 'survey-core';
 import { Survey } from 'survey-react-ui';
 import { SharpDark, SharpLight } from 'survey-core/themes';
+import { useState,useEffect } from 'react';
 import { surveyJSON } from '@/components/survey_json'; // Import the survey JSON from the file
+import { useTheme } from 'next-themes';
 
 export default function SurveyForm() {
+  const {theme,setTheme} = useTheme();
+  const [isDarkMode,setIsDarkMode] = useState(false);
+  useEffect(() => {
+    const systemDark = window.matchMedia('(prefers-color-scheme: dark').matches;
+    setIsDarkMode(theme === 'dark' || (theme === 'system' && systemDark));
+  }, [theme]);
+
   const model = new Model(surveyJSON);
-  model.applyTheme(SharpLight);
-  return <Survey model={model}></Survey>;
+  useEffect(() => {
+    if (isDarkMode) {
+      model.applyTheme(SharpDark);
+    } else {
+      model.applyTheme(SharpLight);
+    }
+  }, [isDarkMode, model]);   
+  return <Survey model={model}/>;
 }
