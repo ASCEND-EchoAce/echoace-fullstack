c7f0960d4e3237b6d49d32863cc71b30a9dc09a6
jer-cortez <realcortez15@gmail.com>
2025-04-21 00:11:12 -0400

    Added user auth to new pages (#31)

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -19,7 +19,7 @@ export default function Navbar({ user }: NavbarProps) {
     : 'Profile';
 
   const links = [
-    ...(user ? [{ url: '/userform', label: 'Survey' }] : [{ url: '/sign-in', label: 'Survey' }]),
+    ...(user ? [{ url: '/dashboard', label: 'Dashboard' }] : [{ url: '/sign-in', label: 'Survey' }]),
     ...(user
       ? [{ url: '/interview', label: 'Interview' }]
       : [{ url: '/sign-in', label: 'Interview' }])
